8189851a9fcaab4da12b6dbd02ebf235313fab90
masyuks <myukimiftakhurrizqi21@gmail.com>
2021-06-14 18:39:20 +0700

    fix status kegiatan

--- application/views/modal/kegiatan.php
+++ application/views/modal/kegiatan.php
@@ -101,7 +101,8 @@
                         <select name="status" class="custom-select form-control" required>
                             <option disabled selected>Pilih Status Kegiatan</option>
                             <option disabled>------------------------------</option>
-                            <option value="Rapat/Sidang Sedang Berlangsung">Rapat/Sidang Sedang Berlangsung</option>
+                            <option value="Rapat Sedang Berlangsung">Rapat Sedang Berlangsung</option>
+                            <option value="Sidang Sedang Berlangsung">Sidang Sedang Berlangsung</option>
                             <option value="Perlu Tindak Lanjut - Sidang Pleno">Perlu Tindak Lanjut - Sidang Pleno
                             </option>
                             <option value="Perlu Tindak Lanjut - Sidang Paripurna">Perlu Tindak Lanjut - Sidang
@@ -338,7 +339,8 @@
                         <?php echo $kegiatan->status;?></option>
                     <option disabled>Pilih Status Kegiatan</option>
                     <option disabled>------------------------------</option>
-                    <option value="Rapat/Sidang Sedang Berlangsung">Rapat/Sidang Sedang Berlangsung</option>
+                    <option value="Rapat Sedang Berlangsung">Rapat Sedang Berlangsung</option>
+                    <option value="Sidang Sedang Berlangsung">Sidang Sedang Berlangsung</option>
                     <option value="Perlu Tindak Lanjut - Sidang Pleno">Perlu Tindak Lanjut - Sidang Pleno</option>
                     <option value="Perlu Tindak Lanjut - Sidang Paripurna">Perlu Tindak Lanjut - Sidang Paripurna
                     </option>
